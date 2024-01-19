21ff5a261fd9209f66fe958e80cd2993153ba9e0
Victor Kabata <victorbro14@gmail.com>
2024-01-19 10:57:25 +0300

    Create WeatherApiService.kt

---
+++ data/network/src/main/java/com/vickbt/network/WeatherApiService.kt
@@ -0,0 +1,25 @@
+package com.vickbt.network
+
+import com.vickbt.network.dtos.CurrentWeatherDto
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+class WeatherApiService(private val weatherApiClient: HttpClient) {
+
+    suspend fun fetchCurrentWeather(
+        query: String,
+        language: String? = "en"
+    ): Flow<CurrentWeatherDto> {
+        return flowOf(
+            weatherApiClient.get("current.json") {
+                parameter("q", query)
+                parameter("lang", language)
+            }.body<CurrentWeatherDto>()
+        )
+    }
+
+}
