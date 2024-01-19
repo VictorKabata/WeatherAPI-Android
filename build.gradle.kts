df581359a443f73c4c8fcfc77ddb0dd01dac3c49
Victor Kabata <victorbro14@gmail.com>
2024-01-19 09:25:30 +0300

    Added ktLint and detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,29 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.ktLint)
+    alias(libs.plugins.detekt)
 }
 true // Needed to make the Suppress annotation work for the plugins block
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    ktlint {
+        debug.set(true)
+        verbose.set(true)
+        android.set(false)
+        outputToConsole.set(true)
+        outputColorName.set("RED")
+        filter {
+            enableExperimentalRules.set(true)
+            exclude { projectDir.toURI().relativize(it.file.toURI()).path.contains("/generated/") }
+            include("**/kotlin/**")
+        }
+    }
+
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    detekt {
+        parallel = true
+        config = files("${project.rootDir}/config/detekt/detekt.yml")
+    }
+}
