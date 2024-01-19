a8a6b0da9ab1df5757e5ab5a85b4411831325458
Victor Kabata <victorbro14@gmail.com>
2024-01-19 11:48:14 +0300

    Set up android application class

---
+++ app/src/main/java/com/vickbt/weatherapiandroid/ui/WeatherApiAndroidApplication.kt
@@ -0,0 +1,25 @@
+package com.vickbt.weatherapiandroid.ui
+
+import android.app.Application
+import com.vickbt.network.di.networkModule
+import com.vickbt.repository.di.repositoryModule
+import com.vickbt.weatherapiandroid.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class WeatherApiAndroidApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val modules = listOf(networkModule, repositoryModule, appModule)
+        startKoin {
+            androidLogger(level = Level.DEBUG)
+            androidContext(this@WeatherApiAndroidApplication)
+            modules(modules)
+        }
+    }
+
+}
