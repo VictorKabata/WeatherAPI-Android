25705fd11cb99a232feebdcb5e9d8e6224284b76
Victor Kabata <victorbro14@gmail.com>
2024-01-20 12:30:20 +0300

    Added api call to fetch history weather

--- data/network/src/main/java/com/vickbt/network/WeatherApiService.kt
+++ data/network/src/main/java/com/vickbt/network/WeatherApiService.kt
@@ -2,12 +2,20 @@ package com.vickbt.network
 
 import com.vickbt.network.dtos.CurrentWeatherDto
 import com.vickbt.network.dtos.ForecastWeatherDto
+import com.vickbt.network.dtos.HistoryForecastDto
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Duration.Companion.days
 
 class WeatherApiService(private val weatherApiClient: HttpClient) {
+
+    private val timeZone = TimeZone.currentSystemDefault()
 
     suspend fun fetchCurrentWeather(
         query: String,
@@ -28,5 +36,19 @@ class WeatherApiService(private val weatherApiClient: HttpClient) {
             parameter("lang", language)
             parameter("days", 7)
         }.body<ForecastWeatherDto>()
+    }
+
+    suspend fun fetchHistoryWeather(
+        query: String,
+        language: String,
+        startDate: LocalDateTime = Clock.System.now().toLocalDateTime(timeZone),
+        endDate: LocalDateTime = Clock.System.now().minus(14.days).toLocalDateTime(timeZone)
+    ): HistoryForecastDto {
+        return weatherApiClient.get("history.json") {
+            parameter("q", query)
+            parameter("lang", language)
+            parameter("dt", "${startDate.year}-${startDate.month}-${startDate.date}")
+            parameter("end_dt", "${endDate.year}-${endDate.month}-${endDate.date}")
+        }.body<HistoryForecastDto>()
     }
 }
