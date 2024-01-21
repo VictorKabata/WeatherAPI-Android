90fe79e2e2e71c49828fe7d2cb4bb32023faf8be
Victor Kabata <victorbro14@gmail.com>
2024-01-21 14:37:05 +0300

    Added logic to request location permission

--- app/src/main/java/com/vickbt/weatherapiandroid/ui/activities/MainActivity.kt
+++ app/src/main/java/com/vickbt/weatherapiandroid/ui/activities/MainActivity.kt
@@ -1,39 +1,193 @@
 package com.vickbt.weatherapiandroid.ui.activities
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.compose.rememberNavController
+import com.vickbt.weatherapiandroid.R
 import com.vickbt.weatherapiandroid.ui.navigation.Navigation
 import com.vickbt.weatherapiandroid.ui.theme.WeatherAPIAndroidTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            var locationPermissionsGranted by remember {
+                mutableStateOf(
+                    areLocationPermissionsAlreadyGranted()
+                )
+            }
+            var shouldShowPermissionRationale by remember {
+                mutableStateOf(
+                    shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)
+                )
+            }
+
+            var shouldDirectUserToApplicationSettings by remember { mutableStateOf(false) }
+
+            var currentPermissionsStatus by remember {
+                mutableStateOf(
+                    decideCurrentPermissionStatus(
+                        locationPermissionsGranted,
+                        shouldShowPermissionRationale
+                    )
+                )
+            }
+
+            val locationPermissions = arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+
+            val locationPermissionLauncher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.RequestMultiplePermissions(),
+                onResult = { permissions ->
+                    locationPermissionsGranted =
+                        permissions.values.reduce { acc, isPermissionGranted ->
+                            acc && isPermissionGranted
+                        }
+
+                    if (!locationPermissionsGranted) {
+                        shouldShowPermissionRationale =
+                            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)
+                    }
+                    shouldDirectUserToApplicationSettings =
+                        !shouldShowPermissionRationale && !locationPermissionsGranted
+                    currentPermissionsStatus = decideCurrentPermissionStatus(
+                        locationPermissionsGranted,
+                        shouldShowPermissionRationale
+                    )
+                })
+
+            val lifecycleOwner = LocalLifecycleOwner.current
+            DisposableEffect(key1 = lifecycleOwner, effect = {
+                val observer = LifecycleEventObserver { _, event ->
+                    if (event == Lifecycle.Event.ON_START &&
+                        !locationPermissionsGranted &&
+                        !shouldShowPermissionRationale
+                    ) {
+                        locationPermissionLauncher.launch(locationPermissions)
+                    }
+                }
+                lifecycleOwner.lifecycle.addObserver(observer)
+                onDispose {
+                    lifecycleOwner.lifecycle.removeObserver(observer)
+                }
+            }
+            )
+
+            val scope = rememberCoroutineScope()
+            val snackbarHostState = remember { SnackbarHostState() }
+
             WeatherAPIAndroidTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val navController = rememberNavController()
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }) {
 
-                    Navigation(navController = navController)
+                        if (shouldShowPermissionRationale) {
+                            LaunchedEffect(Unit) {
+                                scope.launch {
+                                    val userAction = snackbarHostState.showSnackbar(
+                                        message = getString(R.string.permission_request_message),
+                                        actionLabel = getString(R.string.request),
+                                        duration = SnackbarDuration.Indefinite,
+                                        withDismissAction = false
+                                    )
+                                    when (userAction) {
+                                        SnackbarResult.ActionPerformed -> {
+                                            shouldShowPermissionRationale = false
+                                            locationPermissionLauncher.launch(locationPermissions)
+                                        }
+
+                                        SnackbarResult.Dismissed -> {
+                                            shouldShowPermissionRationale = false
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if (shouldDirectUserToApplicationSettings) {
+                            openApplicationSettings()
+                        }
+
+                        if (locationPermissionsGranted) {
+                            val navController = rememberNavController()
+                            Navigation(navController = navController)
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+fun Context.areLocationPermissionsAlreadyGranted(): Boolean {
+    return ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+}
+
+fun Context.openApplicationSettings() {
+    Intent(
+        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+        Uri.fromParts("package", "com.vickbt.weatherapiandroid", null)
+    ).also {
+        startActivity(it)
+    }
+}
+
+private fun decideCurrentPermissionStatus(
+    locationPermissionsGranted: Boolean,
+    shouldShowPermissionRationale: Boolean
+): String {
+    return if (locationPermissionsGranted) "Granted"
+    else if (shouldShowPermissionRationale) "Rejected"
+    else "Denied"
+}
+
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Preview() {
     WeatherAPIAndroidTheme {
         Navigation(navController = rememberNavController())
     }
