229592e3267097bba6c1dbd9d89850202a98da73
Victor Kabata <victorbro14@gmail.com>
2024-01-22 04:28:20 +0300

    Update build.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -14,17 +14,7 @@ plugins {
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
-    // kotlin.applyDefaultHierarchyTemplate()
-
     androidTarget()
-
-    val iosTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
-        when {
-            System.getenv("SDK_NAME")?.startsWith("iphoneos") == true -> ::iosArm64
-            System.getenv("NATIVE_ARCH")?.startsWith("arm") == true -> ::iosSimulatorArm64
-            else -> ::iosX64
-        }
-    iosTarget("ios") {}
 
     sourceSets {
         sourceSets["commonMain"].dependencies {
